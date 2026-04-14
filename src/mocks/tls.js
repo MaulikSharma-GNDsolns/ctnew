aa93b4960758f5a76c614a2b82529841b0a4a6c1
Maulik Sharma <maulik.sharma@gndsolutions.in>
2026-04-14 10:42:43 +0530

    Revert "cloud_on_button"
    This reverts commit 9d8cb0ac43c3717c6f98e004729d658b7a1abde8.

--- src/mocks/tls.js
+++ src/mocks/tls.js
@@ -1,39 +1,20 @@
-// Shim for the 'tls' module using react-native-tcp-socket
-// This allows aws-iot-device-sdk to establish real TLS connections in React Native
-const TcpSocket = require('react-native-tcp-socket');
-
+// Mock for the 'tls' module for React Native
 module.exports = {
-  connect: (options, callback) => {
-    // Map Node.js tls.connect options to react-native-tcp-socket TLS options
-    const tlsOptions = {
-      host: options.host || options.servername,
-      port: options.port || 8883,
-      tls: true,
-      tlsCert: options.cert,
-      tlsKey: options.key,
-      tlsCa: options.ca,
+  connect: () => {
+    return {
+      on: () => {},
+      once: () => {},
+      emit: () => {},
+      end: () => {},
+      destroy: () => {},
+      write: () => {},
     };
-
-    console.log('[TLS Shim] Connecting to:', tlsOptions.host, ':', tlsOptions.port);
-
-    const socket = TcpSocket.createConnection(tlsOptions, () => {
-      console.log('[TLS Shim] TLS connection established');
-      if (callback) callback();
-    });
-
-    // Shim: Node.js tls expects `authorized` property
-    socket.authorized = true;
-
-    return socket;
   },
-  Server: function () {
+  Server: function() {
     return {
       listen: () => {},
       on: () => {},
     };
   },
-  createSecureContext: (options) => {
-    // Return the options as-is; the actual TLS context is handled by the native layer
-    return options || {};
-  },
+  createSecureContext: () => ({}),
 };
